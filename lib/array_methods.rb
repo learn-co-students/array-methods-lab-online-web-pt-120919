f47285b2ee2b0f1f2ea9e41ffb067cde612e148c
christopher tate <chris29tate2@hotmail.co.uk>
2019-12-15 00:39:04 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
-def using_include(array, element)
-
+def array [ways_of_transport]
+"cars, train bus plane"
 end
 
-def using_sort(array)
-
+def (ways_of_transport)
+ ways_of_transport.sort
 end
 
-def using_reverse(array)
-
+def using_reverse( ways_of_transport)
+ways_of_transport.using_reverse
+"plane, train, bus, plane"
 end
 
-def using_first(array)
-
+def using_first(ways_of_transport)
+ways_of_transport.first
+"cars"
 end
 
-def using_last(array)
-
+def using_last(ways_of_transport)
+ways_of_transport.last
+"planes"
 end
 
-def using_size(array)
-
+def using_size(ways_of_transport)
+ways_of_transport.size
+(4)
 end
